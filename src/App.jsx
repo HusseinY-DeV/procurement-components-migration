4cc247dd7e7bc170b99e53f6fdf658e45511561c
Al Husseiny <Hussein.alhusseiny@ctsjounieh.com>
2022-04-04 15:28:42 +0300

    Adding the check box component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
-import { PDFDocument } from "./components/PDFDocument";
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      <PDFDocument />
     </div>
   );
 }

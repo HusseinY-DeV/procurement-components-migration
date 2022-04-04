65f0c93673ead4bfd156eec5f6bdd3af9b9184f6
Ali El Husseini <ali.elhusseini@ctsjounieh.com>
2022-04-04 11:46:34 +0300

    Input Pass Comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import InputPassComp from './components/InputPassComp';
 
 function App() {
 
   return (
     <div className="App">
+      <InputPassComp value='Hello World' />
     </div>
   );
 }
